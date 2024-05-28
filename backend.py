47e729bffd96fef236c742b8a5587305622c4414
Sergey Ivanov <aureolu@gmail.com>
2024-05-28 17:41:28 +0300

    remove get_question into backend.py

--- backend.py
+++ backend.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from datetime import timedelta
 from config import BOT_TOKEN, OPENAI_API_KEY, SYSTEM_PROMPT
 from openai import OpenAI
+import random
 
 TYPE = ("text", "audio", "empty")
 
@@ -82,8 +83,35 @@ def get_report(user_id):
 
 # Получение вопроса
 def get_question(user_id):
-    set_timer(user_id)
-    return "Что такое SOLID?"
+    try:
+        conn = sqlite3.connect('sqlite.db')  # Подключение к базе данных
+        cursor = conn.cursor()  # Создание объекта курсора для выполнения SQL-запросов
+
+        query = '''
+        SELECT q.id, q.name 
+        FROM question q
+        LEFT JOIN user_stat us ON q.id = us.question_id AND us.user_id = ? AND us.correct = 1
+        WHERE us.question_id IS NULL AND q.active = 1
+        '''  # SQL-запрос для выборки случайного вопроса, который пользователь еще не отвечал
+
+        cursor.execute(query, (user_id,))  # Выполнение SQL-запроса с передачей параметра user_id
+        questions = cursor.fetchall()  # Получение всех строк результата запроса
+
+        if not questions:  # Если список вопросов пустой
+            return "Все вопросы были уже правильно отвечены или нет доступных активных вопросов."
+
+        question_id, question_text = random.choice(questions)  # Выбор случайного вопроса из списка
+
+        set_timer(user_id)
+
+        return question_text  # Возвращает текст случайного вопроса
+
+    except sqlite3.Error as e:  # Обработка исключения SQLite
+        print("Ошибка SQLite:", e)  # Вывод сообщения об ошибке
+        return "Произошла ошибка при работе с базой данных. Пожалуйста, попробуйте позже."
+
+    finally:
+        conn.close()  # Закрытие соединения с базой данных в любом случае, даже если возникло исключение
 
 
 # Получение ответа (ChatGPT)
